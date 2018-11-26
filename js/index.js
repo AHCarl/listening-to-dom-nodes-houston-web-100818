b4fdc787f07f417a7272b7e47a6500182ed3ea5f
Alexander Carlin <ahcarlin@gmail.com>
2018-11-26 02:43:38 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -9,5 +9,9 @@ main.addEventListener('click', function(event) {
 });
 
 input.addEventListener('keydown', function(e){
+  if (e.which == 71) {
+    return e.preventDefault()
+  } else {
   console.log(e.which);
+  }
 });
